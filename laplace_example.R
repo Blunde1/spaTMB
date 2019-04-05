bf10951e2acae4bc26f0049ad4cbea72219b7771
Blunde1 <lundeberent@gmail.com>
2019-04-05 22:38:35 +0200

    adding standard tmb laplace examples

---
+++ laplace_example.R
@@ -0,0 +1,32 @@
+## Simulate data
+set.seed(123)
+n <- 10000
+sigma <- .3
+phi <- .8
+simdata <- function(){
+    u <- numeric(n)
+    u[1] = rnorm(1)
+    if(n>=2)
+        for(i in 2:n){
+            u[i] = phi * u[i-1] + rnorm(1, sd = sqrt(1 - phi^2))
+        }
+    u <- u * sigma
+    x <- as.numeric( rbinom(n, 1, plogis(u)) )
+    data <- list(obs=x)
+    data
+}
+##
+data <- simdata()
+parameters <- list(phi=phi, logSigma=log(sigma))
+
+## Adapt parameter list
+parameters$u <- rep(0,n)
+
+require(TMB)
+compile('laplace.cpp')
+dyn.load(dynlib('laplace'))
+
+obj <- MakeADFun(data, parameters, random="u", DLL="laplace")
+obj$fn()
+
+system.time( opt <- nl
